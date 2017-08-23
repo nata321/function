78defa4e96e7cd9bb52916fd92f4ab9c83c73e24
Nataliya <https.ka@gmail.com>
2017-08-23 11:06:46 +0000

    last

--- js/index.js
+++ js/index.js
@@ -1,19 +1,11 @@
 
-const rawA = prompt('Enter number a');
-if (rawA !== null) {
-    const a = parseFloat(rawA);
-    const rawB = prompt('Enter number b');
-    if (rawB !== null) {
-        const b = parseFloat(rawB);
-        const rawC = prompt('Enter number c');
-        if(rawC !== null){
-            const c = parseFloat(rawC);
-            const solution = solvQuadr(a, b, c)
-            document.write(solution);
-        }
-    }
-}
+const a = parseFloat(prompt('Enter number a'));
+const b = parseFloat(prompt('Enter number b'));
+const c = parseFloat(prompt('Enter number c'));
 
+const solution = solvQuadr(a, b, c)
+document.write(solution);
+            
 function solvQuadr(a, b, c) {
     const d = b * b - 4 * a * c;
 
